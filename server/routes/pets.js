8a16f94a50aeb8ca59d700c020f8169e3604acb0
Kat <littlepawcoding@gmail.com>
2019-10-24 17:37:54 +0200

    updated dummy data

--- server/routes/pets.js
+++ server/routes/pets.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/pet/:id', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get("/pet/:id", function(req, res) {
+  res.render("pets", { title: "Express" });
 });
 
 module.exports = router;
