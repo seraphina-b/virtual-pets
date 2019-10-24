5c98cb8c9dad801cef043d476a79194c4fbbf83e
ncgmph2014 <nicole.gathany@gmail.com>
2019-10-24 13:01:06 -0400

    fixed the post statement, but 'blob' needs to be replaced with frontend set data

--- server/model/migration.js
+++ server/model/migration.js
@@ -7,25 +7,25 @@ const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 
 const con = mysql.createConnection({
-  host: DB_HOST || "127.0.0.1",
-  user: DB_USER || "root",
-  password: DB_PASS || "",
-  database: DB_NAME || "virtualPet",
-  multipleStatements: true
+    host: DB_HOST || "127.0.0.1",
+    user: DB_USER || "root",
+    password: DB_PASS || "root",
+    database: DB_NAME || "virtualPet",
+    multipleStatements: true
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  //change this
-  let sql =
-    "DROP TABLE if exists pets; CREATE TABLE pets(petID INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), dateCreated DATETIME, PRIMARY KEY (petID));";
-  con.query(sql, function(err, result) {
+con.connect(function (err) {
     if (err) throw err;
-    console.log("Table creation `pets` was successful!");
+    console.log("Connected!");
+    //change this
+    let sql =
+        "DROP TABLE if exists pets; CREATE TABLE pets(petID INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), dateCreated DATETIME, PRIMARY KEY (petID));";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table creation `pets` was successful!");
 
-    console.log("Closing...");
-  });
+        console.log("Closing...");
+    });
 
-  con.end();
+    con.end();
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ var db = require("../model/helper");
 
 /* GET home page. */
 function sendItems(req, res) {
-  db("SELECT * FROM items ORDER BY id ASC;").then(results => {
+  db("SELECT * FROM pets").then(results => {
     if (results.error) {
       res.status(500).send(results.error);
     }
@@ -30,8 +30,19 @@ router.get("/", (req, res) => {
 //then add the INSERT thing into the put request
 
 //use postman to test post
+//does not work
 router.post("/", (req, res) => {
-  db(`INSERT INTO pets (name, dateCreated) VALUES ("Blob", NOW());`)
+  //will change the "Blob" to whatever is on the frontend
+  db(`INSERT INTO pets (name, dateCreated) VALUES ("Blob", NOW());`).then(
+    //not pushing because we're using SQL, bitch.
+    //the SQL code is basically inserting the values into the table like we would type something into an Excel sheet wow
+    results => {
+      if (results.error) {
+        res.status(500).send(results.error);
+      }
+      sendItems(req, res);
+    }
+  );
 }
 
 
