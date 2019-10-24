fc4b5c93a900c07693d52895cb763da0250bf9c1
admin <admin@admins-MacBook-Pro.local>
2019-10-24 15:38:18 +0200

    useless frontend but at least it exists right?

---
+++ react/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Virtual Pet</h1> 
+        <button>Create New Pet</button>
+        <Header></Header>
+      </div> 
+    )
+  }
+}
+export default App;
---
+++ react/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {} 
+    }
+        render() {
+            return (
+                <p>fuck</p>
+            )
+        }
+    }
+
+    export default Header;
