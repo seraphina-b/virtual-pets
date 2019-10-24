c275a33b586e1e7ad0bf4e117a84b5ccdcf3c507
Kat <littlepawcoding@gmail.com>
2019-10-24 17:58:05 +0200

    added events table to migration file

--- server/model/migration.js
+++ server/model/migration.js
@@ -34,5 +34,14 @@ con.connect(function(err) {
     console.log("Dummy data added to pets table");
   });
 
+  let sql =
+    "DROP TABLE if exists events; CREATE TABLE events(eventID INT NOT NULL AUTO_INCREMENT, petID INT, activity VARCHAR(255), PRIMARY KEY(eventID));";
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Table creation `events` was successful!");
+
+    console.log("Closing...");
+  });
+
   con.end();
 });
