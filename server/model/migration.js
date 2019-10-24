47a13828d8a3ffdc5c17640c64c47284eff0054c
ncgmph2014 <nicole.gathany@gmail.com>
2019-10-24 13:01:06 -0400

    fixed the post statement, but 'blob' needs to be replaced with frontend set data

--- server/model/migration.js
+++ server/model/migration.js
@@ -7,49 +7,55 @@ const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 
 const con = mysql.createConnection({
-  host: DB_HOST || "127.0.0.1",
-  user: DB_USER || "root",
-  password: DB_PASS || "",
-  database: DB_NAME || "virtualPet",
-  multipleStatements: true
+    host: DB_HOST || "127.0.0.1",
+    user: DB_USER || "root",
+    password: DB_PASS || "root",
+    database: DB_NAME || "virtualPet",
+    multipleStatements: true
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  // create pets table
-  let sql =
-    "DROP TABLE if exists pets; CREATE TABLE pets(petID INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), dateCreated DATETIME, PRIMARY KEY (petID));";
-  con.query(sql, function(err, result) {
+con.connect(function (err) {
     if (err) throw err;
-    console.log("Table creation `pets` was successful!");
+    console.log("Connected!");
+    // create pets table
+    let sql =
+        "DROP TABLE if exists pets; CREATE TABLE pets(petID INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), dateCreated DATETIME, PRIMARY KEY (petID));";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Connected!");
+        //change this
+        let sql =
+            "DROP TABLE if exists pets; CREATE TABLE pets(petID INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), dateCreated DATETIME, PRIMARY KEY (petID));";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table creation `pets` was successful!");
 
-    console.log("Closing...");
-  });
+            console.log("Closing...");
+        });
 
-  // create dummy data for pets table
-  sql = "INSERT INTO `pets` (name, dateCreated) VALUES ('Blob', NOW())";
-  con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log("Dummy data added to pets table");
-  });
+        // create dummy data for pets table
+        sql = "INSERT INTO `pets` (name, dateCreated) VALUES ('Blob', NOW())";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Dummy data added to pets table");
+        });
 
-  // create events table
-  sql =
-    "DROP TABLE if exists events; CREATE TABLE events(eventID INT NOT NULL AUTO_INCREMENT, petID INT, activity VARCHAR(255), PRIMARY KEY(eventID));";
-  con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log("Table creation `events` was successful!");
+        // create events table
+        sql =
+            "DROP TABLE if exists events; CREATE TABLE events(eventID INT NOT NULL AUTO_INCREMENT, petID INT, activity VARCHAR(255), PRIMARY KEY(eventID));";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table creation `events` was successful!");
 
-    console.log("Closing...");
-  });
+            console.log("Closing...");
+        });
 
-  // create dummy data for events table
-  sql = "INSERT INTO `events` (petID, activity) VALUES (1, 'lastFed')";
-  con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log("Dummy data added to events table");
-  });
+        // create dummy data for events table
+        sql = "INSERT INTO `events` (petID, activity) VALUES (1, 'lastFed')";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Dummy data added to events table");
+        });
 
-  con.end();
-});
+        con.end();
+    });
