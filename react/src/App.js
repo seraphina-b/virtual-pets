205c7009176bf3cc0b1b0bc20802e3497d88ac02
admin <admin@admins-MacBook-Pro.local>
2019-10-29 15:02:35 +0100

    installed nes

--- react/src/App.js
+++ react/src/App.js
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 import React from "react";
 import Header from "./views/Pet/Header.js";
 import Footer from "./views/Pet/Footer.js";
 import LogIn from "./views/LogIn.js";
+=======
+import React from 'react';
+import Header from './views/Pet/Header.js';
+import Footer from './views/Pet/Footer.js';
+import LogIn from './views/LogIn.js';
+>>>>>>> installed nes
 import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends React.Component {
@@ -11,6 +18,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+<<<<<<< HEAD
         <div>
           <Header></Header>
           <h1>Virtual Pet</h1>
@@ -20,6 +28,17 @@ class App extends React.Component {
         </div>
       </BrowserRouter>
     );
+=======
+      <div>
+        <Header></Header>
+        <h1>Virtual Pet</h1> 
+        <button>Create New Pet</button>
+        <Footer></Footer>
+        <Route path="/LogIn" component={LogIn} />
+      </div> 
+      </BrowserRouter>
+    )
+>>>>>>> installed nes
   }
 }
 export default App;
