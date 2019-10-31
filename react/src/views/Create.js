e304cbd5b556ba35ddcb11d5e7589ba46a9b781a
Kat <littlepawcoding@gmail.com>
2019-10-31 22:24:13 +0100

    trying to add age to create pet

--- react/src/views/Create.js
+++ react/src/views/Create.js
@@ -1,33 +1,55 @@
-import React from 'react';
-import NewPet from "./NewPet.js"
-
+import React from "react";
+import NewPet from "./NewPet.js";
 
 class Create extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNewPet: false,
+      getAge: []
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isNewPet: false
-        };
-    }
+  handleClick = () => {
+    this.setState(prevState => ({
+      isNewPet: !prevState.isNewPet
+    }));
+    console.log(this.state.isNewPet);
+  };
 
+  componentDidMount() {
+    fetch("/:petID/age")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          getAge: data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    handleClick = () => {
-        this.setState(prevState => ({
-            isNewPet: !prevState.isNewPet
-        }));
-            console.log(this.state.isNewPet);
-    }
-
-  
-    render() {
-        return (
-            <body>
-                <button type="button" onClick={this.handleClick} className="nes-btn is-primary">Create New Pet</button>
-                {this.state.isNewPet ? <NewPet/> : null } 
-            </body>
-        )
-    }
+  render() {
+    return (
+      <body>
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="nes-btn is-primary"
+        >
+          Create New Pet
+        </button>
+        {this.state.isNewPet ? <NewPet /> : null}
+        {/* Note: I'm trying to make it so that when the submit button is clicked it will display the age of the pet using the get request made in pets.js, but it ain't working. *shrugs* */}
+        <div>
+          {this.state.getAge.map(age => {
+            return { age };
+          })}
+        </div>
+      </body>
+    );
+  }
 }
 
 export default Create;
