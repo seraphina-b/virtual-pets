f9d32602c82e8deed6a6d5be8c2f9301d6b4da00
Kat <littlepawcoding@gmail.com>
2019-10-31 17:03:06 +0100

    route changes

--- server/routes/events.js
+++ server/routes/events.js
@@ -15,34 +15,4 @@ router.get("/events", (req, res) => {
   });
 });
 
-// GET lists name, activity and timeActioned by petID
-router.get("/pets/:petID/events", (req, res) => {
-  db(`SELECT p.name, e.activity, e.timeActioned FROM events AS e
-LEFT JOIN pets AS p
-ON e.petID = p.petID
-WHERE p.petID=${req.params.petID}`).then(results => {
-    if (results.error) {
-      res.status(500).send(results.error);
-    }
-    res.send(results.data);
-  });
-});
-
-//POST feeds a pet
-router.post("/pets/:petID/events", (req, res) => {
-  db(
-    `INSERT INTO events (petID, activity, timeActioned) VALUES (${req.params.petID}, 'lastfed', NOW());`
-  ).then(results => {
-    if (results.error) {
-      res.status(500).send(results.error);
-    }
-    db(`UPDATE pets SET satiety = satiety+2 WHERE petID = 1;`).then(results => {
-      if (results.error) {
-        res.status(500).send(results.error);
-      }
-    });
-    res.send({ message: "baby was fed!" });
-  });
-});
-
 module.exports = router;
