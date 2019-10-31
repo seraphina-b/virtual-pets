e38f9630d55955a43e4c6d401fc845f5473545d1
ncgmph2014 <nicole.gathany@gmail.com>
2019-10-31 16:56:08 +0100

    adding user tables to migration files

--- react/src/views/Pet/Pet.js
+++ react/src/views/Pet/Pet.js
@@ -5,36 +5,37 @@ import eggStage from "../.././images/lifeStages/eggStage.png";
 import baby from "../.././images/lifeStages/baby.png";
 
 class Pet extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            satiety: 11
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      satiety: 11
+    };
+  }
 
-    componentDidMount = () => {
-        
-    }
+  componentDidMount = () => {
 
-    render() {
-        return (
-        <body>
-          <h3>Hunger</h3>
-          <progress className="nes-progress is-success" max="15" value={this.state.satiety}/> 
-          <h1>Virtual Pet</h1>
-          <Header></Header>
-          <br></br>
-          <section className="nes-container with-title"> 
-            <h3 className="title">Your Pet</h3>
-            <i className="nes-icon heart is-large"></i>
-            <i className="nes-icon heart is-large"></i>
-            <i className="nes-icon heart is-large"></i> 
-            <img src={eggStage} alt="Egg tamagotchi"></img>
-          </section>
-          
-          
-          </body>
-        )
-    }
+  }
+  //just writing a note so i'll understand
+  // i tag is an icon tag. didn't know that before
+  render() {
+    return (
+      <body>
+        <h3>Hunger</h3>
+        <progress className="nes-progress is-success" max="15" value={this.state.satiety} />
+        <h1>Virtual Pet</h1>
+        <Header></Header>
+        <br></br>
+        <section className="nes-container with-title">
+          <h3 className="title">Your Pet</h3>
+          <i className="nes-icon heart is-large"></i>
+          <i className="nes-icon heart is-large"></i>
+          <i className="nes-icon heart is-large"></i>
+          <img src={eggStage} alt="Egg tamagotchi"></img>
+        </section>
+
+
+      </body>
+    )
+  }
 }
- export default Pet;
+export default Pet;
