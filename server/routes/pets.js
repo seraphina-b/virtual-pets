f7e3a788191885297493e591f1f062b9280eb5bf
Kat <littlepawcoding@gmail.com>
2019-10-31 16:41:37 +0100

    moved endpoints from events to pets

--- server/routes/pets.js
+++ server/routes/pets.js
@@ -25,7 +25,6 @@ router.get("/:petID", (req, res) => {
   });
 });
 
-
 // not sure what this post is doing, but it works!
 router.post("/", (req, res) => {
   db(
@@ -43,6 +42,36 @@ router.put("/:petID", (req, res) => {
     if (results.error) {
       res.status(500).send(results.error);
     }
+  });
+});
+
+// GET lists name, activity and timeActioned by petID
+router.get("/pets/:petID/events", (req, res) => {
+  db(`SELECT p.name, e.activity, e.timeActioned FROM events AS e
+LEFT JOIN pets AS p
+ON e.petID = p.petID
+WHERE p.petID=${req.params.petID}`).then(results => {
+    if (results.error) {
+      res.status(500).send(results.error);
+    }
+    res.send(results.data);
+  });
+});
+
+//POST feeds a pet
+router.post("/pets/:petID/events", (req, res) => {
+  db(
+    `INSERT INTO events (petID, activity, timeActioned) VALUES (${req.params.petID}, 'lastfed', NOW());`
+  ).then(results => {
+    if (results.error) {
+      res.status(500).send(results.error);
+    }
+    db(`UPDATE pets SET satiety = satiety+2 WHERE petID = 1;`).then(results => {
+      if (results.error) {
+        res.status(500).send(results.error);
+      }
+    });
+    res.send({ message: "baby was fed!" });
   });
 });
 
