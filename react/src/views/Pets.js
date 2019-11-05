811779d036e3840bdce37866e06a945464810c00
ncgmph2014 <nicole.gathany@gmail.com>
2019-11-05 13:52:56 +0100

    working on Pets page navigation. not at a good stopping point but i'm hungry

--- react/src/views/Pets.js
+++ react/src/views/Pets.js
@@ -1,6 +1,14 @@
 //Pets (plural) frontend page
 //not to be confused with Pet (singluar)
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+} from "react-router-dom";
 
 class Pets extends React.Component {
     constructor(props) {
@@ -9,10 +17,27 @@ class Pets extends React.Component {
     }
 
     //nicole note: sort of nervous because the number of put pages will increase with use creation
+    //may need a map function to map through like in sheba's project
+    //it doesn't know which id we're talking about yet, shows up too literally
     render() {
         return (
             <div>
                 <h1>Pets Page</h1>
+                <Router>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link to="/pet/:id">Pet name</Link>
+                            </li>
+
+                        </ul>
+
+                        <Switch>
+
+
+                        </Switch>
+                    </div>
+                </Router>
 
             </div>
         )
