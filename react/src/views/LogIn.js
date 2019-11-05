e657bc9448c4b076002ab3cab4b1a13e40994e95
Kat <littlepawcoding@gmail.com>
2019-11-05 12:28:41 +0100

    added bootstrap styling & comments

--- react/src/views/LogIn.js
+++ react/src/views/LogIn.js
@@ -9,12 +9,15 @@ class LogIn extends React.Component {
   render() {
     return (
       <div>
+        {/* Main section  - user can choose to login or create pet*/}
         <section className="nes-container with-title">
           <h1>Virtual Pets</h1>
-          <button className="nes-btn is-primary">Adopt</button>
+          <button className="nes-btn is-primary">Create Pet</button>
           <button className="nes-btn is-primary">Login</button>
         </section>
+        {/* added a br for a bit of spacing between the sections - but not sure if this is best practice */}
         <br></br>
+        {/* Login section - users with an account - takes them to pets page (list of pets) */}
         <section className="nes-container with-title">
           <h3 className="title">Log In</h3>
           <h4>Email</h4>
@@ -24,8 +27,9 @@ class LogIn extends React.Component {
           <button className="nes-btn is-primary">Log In</button>
         </section>
         <br></br>
+        {/* Create pet section - users sign up to create a pet - takes them to create pet page*/}
         <section className="nes-container with-title">
-          <h3 className="title">Adopt</h3>
+          <h3 className="title">Create Pet</h3>
           <h4>Email</h4>
           <input className="nes-input"></input>
           <h4>Password</h4>
