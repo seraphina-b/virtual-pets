a7fd9d2fe1767cf3f4a0ac5623c3d770b5670b1b
Kat <littlepawcoding@gmail.com>
2019-11-05 12:12:47 +0100

    new sections for login page

--- react/src/views/LogIn.js
+++ react/src/views/LogIn.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React from "react";
 
 class LogIn extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <section className="nes-container with-title">
-                    <h3 className="title">Log In</h3>
-                    <h4>Email</h4>
-                    <input className="nes-input"></input>
-                    <h4>Password</h4>
-                    <input className="nes-input"></input>
-                    <button className="nes-btn is-primary">Log In</button>
-                    <button className="nes-btn is-primary">Sign Up</button>
-                    
-                </section>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <section className="nes-container with-title">
+          <h1>Virtual Pets</h1>
+          <button className="nes-btn is-primary">Adopt</button>
+          <button className="nes-btn is-primary">Login</button>
+        </section>
+        <br></br>
+        <section className="nes-container with-title">
+          <h3 className="title">Log In</h3>
+          <h4>Email</h4>
+          <input className="nes-input"></input>
+          <h4>Password</h4>
+          <input className="nes-input"></input>
+          <button className="nes-btn is-primary">Log In</button>
+        </section>
+        <br></br>
+        <section className="nes-container with-title">
+          <h3 className="title">Adopt</h3>
+          <h4>Email</h4>
+          <input className="nes-input"></input>
+          <h4>Password</h4>
+          <input className="nes-input"></input>
+          <button className="nes-btn is-primary">Sign Up</button>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default LogIn;
