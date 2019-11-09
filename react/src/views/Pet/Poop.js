39ff3f91d751a16e5bf5280c2d238cec8aeed986
ncgmph2014 <nicole.gathany@gmail.com>
2019-11-09 12:37:32 +0100

    fixed Poop component so at the very least it won't mess up the other files

--- react/src/views/Pet/Poop.js
+++ react/src/views/Pet/Poop.js
@@ -16,41 +16,7 @@ class Poop extends React.Component {
         };
     }
 
-    // function to connect to pusher
-    // this is a cron job controlled in petBars and sendNotifications
-    // connectToPusher = () => {
-    //     var pusher = new Pusher("a6e425669a496f8c754a", {
-    //         cluster: "eu",
-    //         forceTLS: true
-    //     });
 
-    //     var channel = pusher.subscribe("my-channel");
-    //     channel.bind("my-event", data => {
-    //         console.log(data);
-    //         this.getData();
-    //     });
-    // };
-
-    componentDidMount = () => {
-        // this.connectToPusher();
-        this.getData();
-    };
-
-    // getData captures the pet data and displays it on the screen
-    // this basically refreshed the page and updates the pet info
-    getData = () => {
-        let id = this.props.match.params.id;
-        fetch(`/pets/${id}`)
-            .then(res => res.json())
-            .then(data => {
-                this.setState({
-                    pet: data
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
 
 
     render() {
@@ -58,7 +24,7 @@ class Poop extends React.Component {
         let poopPic;
         if (this.state.pet.foodTime <= "00:0:30") {
             poopPic = <img src={poop} alt="poop"></img>;
-        } else poopPic = NULL;
+        }
 
 
         return (
